c92a6f03389a18629d588ab2814145f33b9e4926
ErikMurmi <erikmurmi@gmail.com>
2023-01-16 19:06:59 -0500

    Diseño de login

--- login-front/src/app/app.component.ts
+++ login-front/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
+
+  constructor(private router: Router) {}
+
   title = 'login-front';
+  
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
