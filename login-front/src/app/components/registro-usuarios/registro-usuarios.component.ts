263b03febedea3138035f49945516c1c327e874b
ErikMurmi <erikmurmi@gmail.com>
2023-01-30 14:26:58 -0500

    buscar y registrar

--- login-front/src/app/components/registro-usuarios/registro-usuarios.component.ts
+++ login-front/src/app/components/registro-usuarios/registro-usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-usuarios',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-usuarios.component.css']
 })
 export class RegistroUsuariosComponent {
+  registerForm: FormGroup
+  constructor() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+      rol: new FormControl('')
+    });
+  }
 
+  onSubmit() {
+    const formData = this.registerForm.value;
+    /** Validar seleccion del rol*/
+    if(formData['rol'] ==""){
+      alert('Se debe seleccionar un rol')
+    }else{
+      /**Implementar la llamada a la api */
+    }
+
+    // this.auth.login(this.loginForm.value)
+    // .subscribe({
+    //   next:(res)=>{        
+    //   },
+    //   error:(err)=>{
+    //     console.log('la respuesta es :',err)
+    //     //alert(err?.error.message)
+    //   }
+    // })
+
+
+    console.log(formData);
+  }
 }
